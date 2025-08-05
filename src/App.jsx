74484b280986959a1717529549e014e5a146d6da
hajar <hajartawfiq007@gmail.com>
2025-08-05 15:02:50 +0300

    TourCardCategories

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css';
 import PublicGroups from './components/PublicGroups';
 import Discover from './components/Discover'  ;
-import CategoriesPage from './components/CategoriesPage';
+import TourCard from './components/TourCardCategories';
+import tourImage from './images/tourcard.png' ;
 
 
 function App() {
   return (
-    <div>      
+    <div>  
+      <div >
+      <TourCard
+        image={tourImage}
+        title="Restaurants"
+        button="142 restaurants"
+        width="300px"
+        height="250px"
+      />
+    </div>
+
       <PublicGroups />
 
       <Discover />
