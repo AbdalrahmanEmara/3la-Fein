92d78efb790ed5f4510b0b8b2d1ded9a65954c38
reem_mustafa <reemmustafa905@gmail.com>
2025-08-06 22:31:02 +0300

    update

--- src/routes.jsx
+++ src/routes.jsx
@@ -3,10 +3,12 @@ import SignUp from "./Pages/Sign/SignUP";
 import SignIn from "./Pages/Sign/SignIn";
 import { createBrowserRouter } from "react-router-dom";
 import Profile from "./Pages/Profile/Profile";
+import NotFound from "./Pages/NotFound";
 
 export const router = createBrowserRouter([
   { path: "/", element: <Home /> },
   { path: "/signup", element: <SignUp /> },
   { path: "/signin", element: <SignIn /> },
   { path: "/profile", element: <Profile /> },
+  { path: "*", element: <NotFound /> },
 ]);
