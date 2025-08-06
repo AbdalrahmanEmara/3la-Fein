f0f36e4594433f234ceafbf2eb3f30f4f9617906
kareem mohamed <karemm420@gmail.com>
2025-08-06 11:14:39 +0300

    fix error

--- src/Componets/Reviews/CurrentReviewHead.jsx
+++ src/Componets/Reviews/CurrentReviewHead.jsx
@@ -3,11 +3,7 @@ import Style from "./ReviewStyle.module.css";
 const CurrentReviewHead = ({ name, job }) => {
   return (
     <div className={Style["current-review-head"]}>
-      <img
-        src="../../public/images/person1.png"
-        alt=""
-        className={Style["avatar"]}
-      />
+      <img src="images/person1.png" alt="" className={Style["avatar"]} />
       <div className={Style["section-name-job"]}>
         <p className={Style["review-profile-name"]}>{name}</p>
         <p className={Style["review-profile-job"]}>{job}</p>
